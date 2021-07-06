8dd241ae313dc09461e1691b5e44ece76480b7d2
itsfuad <fuad.cs22@gmail.com>
2021-07-06 15:36:48 +0600

    sep

---
+++ loader.js
@@ -0,0 +1,11 @@
+
+
+function load_home(){
+
+  document.getElementsByTagName("header")[0].classList.add("loaded");
+}
+
+function load(){
+    document.getElementById("load").classList.toggle("done");
+    load_home();
+}
--- script.js
+++ script.js
@@ -41,14 +41,3 @@ contact.onclick = () => {
     document.getElementsByClassName("contact_me")[0].scrollIntoView(true);
    // window.location.href = url + "#contact";
 }
-
-
-function load_home(){
-
-  document.getElementsByTagName("header")[0].classList.add("loaded");
-}
-
-function load(){
-    document.getElementById("load").classList.toggle("done");
-    load_home();
-}
