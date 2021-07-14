4331d0b618b14616f6b0f2754bc4dbf19fd7aa8f
Fuad Hasan <fuad.cs22@outlook.com>
2021-07-14 17:22:34 +0600

    gtr

--- blogs/videos.js
+++ blogs/videos.js
@@ -3,12 +3,15 @@ var channelID = "UCsBjURrPoezykLs9EqgamOA";
     $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
        var link = data.items[0].link;
        var id = link.substr(link.indexOf("=")+1);
-    $("#youtube_video").attr("src","https://youtube.com/embed/"+ id + "?controls=0&showinfo=0&rel=0");
+    $("#youtube_video").attr("src","https://youtube.com/embed/"+ id + "?controls=1&showinfo=0&rel=0");
 		var link = data.items[1].link;
        	var id = link.substr(link.indexOf("=")+1);
-    $("#youtube_video2").attr("src","https://youtube.com/embed/"+ id + "?controls=0&showinfo=0&rel=0");
+    $("#youtube_video2").attr("src","https://youtube.com/embed/"+ id + "?controls=1&showinfo=0&rel=0");
 		var link = data.items[2].link;
      	var id = link.substr(link.indexOf("=")+1);
-    $("#youtube_video3").attr("src","https://youtube.com/embed/"+ id + "?controls=0&showinfo=0&rel=0");
-	
+    $("#youtube_video3").attr("src","https://youtube.com/embed/"+ id + "?controls=1&showinfo=0&rel=0");
+    var link = data.items[3].link;
+    var id = link.substr(link.indexOf("=")+1);
+    $("#youtube_video4").attr("src","https://youtube.com/embed/"+ id + "?controls=1&showinfo=0&rel=0");
+
     });
