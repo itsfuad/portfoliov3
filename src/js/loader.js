78a27078f558538b22853399d6dacf042d88739a
Fuad Hasan <fuad.cs22@outlook.com>
2021-08-02 14:02:58 +0600

    too much

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,39 @@
 
 const load_home = () => {
   document.getElementsByTagName("header")[0].classList.add("loaded");
-  agent();
+  //agent();
 }
 
 const load = () => {
     document.getElementById("load").classList.toggle("done");
     load_home();
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+  function lazyload () {
+    console.log("💩");
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }    
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+  }
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
